c3bbef78582e9b6b3b6031c98c363c607eed4459
maikthulhu <maik@halfzero.net>
2017-12-07 08:20:28 -0700

    Changed print formatting to .format() syntax and added argparse with --brief

--- print_nmapxml_scripts_results.py
+++ print_nmapxml_scripts_results.py
@@ -1,30 +1,51 @@
 # Needs python-libnmap
 import sys
+import argparse
+
 from libnmap.parser import NmapParser
 
 def usage(script_name):
     print "Nmap XML Script Results Parser"
+    print "by: maik"
     print
-    print "Usage: %s report_file.xml"
+    print "Usage: %s [--brief | -b] report_file.xml"
     
 def main():
-    if not len(sys.argv[1:]):
-        usage(sys.argv[0])
-        sys.exit(0)
-        
-    parser = NmapParser.parse_fromfile(sys.argv[1])
+    argp = argparse.ArgumentParser(description='''
+Nmap XML Script Results Parser
+by: maik''',
+                                   formatter_class=argparse.RawTextHelpFormatter)
+    argp.add_argument('report_file', help='XML-formatted Nmap output')
+    argp.add_argument('-b', '--brief', action='store_true', help='Brief output')
+    args = argp.parse_args()
+
+    parser = NmapParser.parse_fromfile(args.report_file)
     for host in parser.hosts:
         print host.id
         for results in host.scripts_results:
-            print "  %s" % results['id']
-            if not results['elements']:
-                print "    %s" % results['output']
+            if not results['elements'] and args.brief:
+                continue
+            elif not results['elements'] and not args.brief:
+                print "  {}".format(results['id'])
+                print "    {}".format(results['output'])
                 print
                 continue
+            else:
+                print "  {}".format(results['id'])
+
+
+
             for element in results['elements']:
-                print "    %s" % element
+                if not args.brief:
+                    print "    {}".format(element)
                 for k,v in results['elements'][element].iteritems():
-                    print "    %s : %s" % (k, v.rstrip())
+                    if args.brief:
+                        if k == 'state':
+                            print "    {} : {}".format(k, v.rstrip())
+                            break
+                        else:
+                            continue
+                    print "    {} : {}".format(k, v.rstrip())
                 
                 print
                 
